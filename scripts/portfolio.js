38626ba96b1cfc9798a2d0aa6ccd6e5493c79d71
Lucas Ogden-Davis <lucasod@gmail.com>
2018-09-02 16:24:54 -0400

    Create project images and add to projects.json

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -6,12 +6,12 @@ Return is a JSON object with each entry in the following format:
   "name": string,
   "description": string,
   "technologies": array of strings,
-  "live-url": string with full url,
-  "repo-url": string with full url,
-  "image-monitor": string with relative url,
-  "image-phone": string with relative url
+  "liveUrl": string with full url,
+  "repoUrl": string with full url,
+  "imageMonitor": string with relative url,
+  "imagePhone": string with relative url
 }
 
-image-main is landscape, 763px x 600px (screenshot is ~680px wide)
-image-phone is portrait,  350px x 713px (screenshot is ~300px wide)
+imageMonitor is landscape, 763px x 600px (screenshot is ~680px wide)
+imagePhone is portrait,  350px x 713px (screenshot is ~300px wide)
 */
