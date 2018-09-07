2da65472635cdfd6c509d42099905f47c9a4242f
Lucas Ogden-Davis <lucasod@gmail.com>
2018-09-07 12:18:49 -0400

    Complete portfolio functionality

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -16,6 +16,11 @@ imageMonitor is landscape, 763px x 600px (screenshot is ~680px wide)
 imagePhone is portrait,  350px x 713px (screenshot is ~300px wide)
 */
 
+/*
+Possible future upgrade: Switch technology list to radio buttons, so user can
+select/view combinations of technologies, instead of just being able to pick 1
+*/
+
 'use strict';
 
 /* This uses a link to the projects.json file in the DEVELOPMENT branch on GitHub. When ready for release,
@@ -37,23 +42,49 @@ getJSON.onload = function() {
 }
 
 function loadPortfolio(projects) {
-  ReactDOM.render(<Portfolio projects={projects} />, document.querySelector('main'))
+  ReactDOM.render(<Portfolio projects={projects} />, document.querySelector('main'));
 }
 
 class PortfolioItem extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      active: false
+      active: this.props.active,
+      tech: this.props.technologies
+    }
+    this.makeTechTags = this.makeTechTags.bind(this);
+  }
+
+  componentDidUpdate() {
+    if (this.props.active !== this.state.active) {
+      this.setState( { active: this.props.active });
     }
   }
 
+  makeTechTags() {
+    const techs = this.props.technologies;
+    const tags = techs.map(tech => {
+      return (
+        <li key={this.props.name + tech}>{tech}</li>
+      )
+    });
+    return (
+      <ul>
+        {tags}
+      </ul>
+    )
+  }
+
   render() {
+    if (!this.state.active) {
+      return null;
+    }
     return (
       <section key={this.props.name + 'Section'}>
-        <h1>{this.props.name}</h1>
+        <h2>{this.props.name}</h2>
         <p>{this.props.description}</p>
-        <p>Technologies used: {this.props.technologies}</p>
+        <p>Technologies used:</p>
+          {this.makeTechTags()}
         <ul>
           <li><a href={this.props.liveUrl}>See live demo</a></li>
           <li><a href={this.props.repoUrl}>See code repo</a></li>
@@ -69,9 +100,65 @@ class Portfolio extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      technologies: 'all'
+      allTech: [],
+      activeTech: []
     }
     this.drawPortfolio = this.drawPortfolio.bind(this);
+    this.listTechnologies = this.listTechnologies.bind(this);
+    this.toggleTech = this.toggleTech.bind(this);
+    this.checkActiveTech = this.checkActiveTech.bind(this);
+  }
+
+  componentDidMount() {
+    const projects = this.props.projects;
+    const allTech = [];
+    Object.keys(projects).forEach(key => {
+      projects[key].technologies.forEach(tech => {
+        if (allTech.indexOf(tech) === -1) {
+          allTech.push(tech);
+        }
+      })
+    });
+    allTech.sort((a,b) => a.toLowerCase() > b.toLowerCase());
+    allTech.unshift('View all');
+    this.setState( {
+      allTech: allTech,
+      activeTech: allTech
+    });
+  }
+
+  listTechnologies() {
+    const techToggles = this.state.allTech.map(tech => {
+      return (
+        <li className="sorter--list--item" id={tech + 'Toggle'} key={tech + 'Toggle'} onClick={this.toggleTech}>{tech}</li>
+      );
+    });
+
+    return (
+      <div className="sorter">
+        Click to see only projects that use:
+        <ul className="sorter--list">
+          {techToggles}
+        </ul>
+      </div>
+    );
+  }
+
+  toggleTech(event) {
+    const allTech = this.state.allTech;
+    const clicked = event.target.id.slice(0, -6);
+    if (clicked === 'View all') {
+      this.setState({ activeTech: allTech });
+    }
+    else {
+      const newActiveTech = [ clicked ];
+      this.setState({ activeTech: newActiveTech });
+    }
+  }
+
+  checkActiveTech(projectTech) {
+    const activeTech = this.state.activeTech;
+    return projectTech.some(tech => activeTech.includes(tech) );
   }
 
   drawPortfolio() {
@@ -79,7 +166,7 @@ class Portfolio extends React.Component {
     const result = [];
     for (let project in projects) {
       result.push(
-        <PortfolioItem key={projects[project].name + "Item"} name={projects[project].name} description={projects[project].description} technologies={projects[project].technologies} liveUrl={projects[project].liveUrl} repoUrl={projects[project].repoUrl} imageMonitor={projects[project].imageMonitor} imagePhone={projects[project].imagePhone} />
+        <PortfolioItem key={projects[project].name + "Item"} active={this.checkActiveTech(projects[project].technologies)} name={projects[project].name} description={projects[project].description} technologies={projects[project].technologies} liveUrl={projects[project].liveUrl} repoUrl={projects[project].repoUrl} imageMonitor={projects[project].imageMonitor} imagePhone={projects[project].imagePhone} />
       );
     }
     return result;
@@ -87,7 +174,8 @@ class Portfolio extends React.Component {
 
   render() {
     return (
-      <div className="container">
+      <div className="portfolio">
+        {this.listTechnologies()}
         {this.drawPortfolio()}
       </div>
     );
