77447331dce0ebf258336cb118d7fdb6a4a8f323
Lucas Ogden-Davis <lucasod@gmail.com>
2018-09-14 15:36:43 -0400

    Add post thumbnails. Done for day.

--- scripts/blog.js
+++ scripts/blog.js
@@ -1,6 +1,6 @@
 /* Pulls blog.json from store. Object format:
     {
-      post number, starting from 001: {
+      post number, zero-indexed: {
         "title": string,
         "image": string with relative URL for featured image,
         "imageOrientation": either "portrait" or "landscape",
@@ -34,12 +34,48 @@ function loadBlog(blog) {
   ReactDOM.render(<Blog posts={blog} />, document.querySelector('.main'));
 }
 
-class Post extends React.Component {
+class ThumbPost extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       active: false
     }
+    this.styleThumb = this.styleThumb.bind(this);
+  }
+
+  componentDidUpdate() {
+    if (this.props.active !== this.state.active) {
+      this.setState( { active: this.props.active } );
+    }
+  }
+
+  styleThumb() {
+    if (this.state.active === true) {
+      return {
+        background: 'red'
+      }
+    }
+    return {
+      background: `url(${this.props.image})`,
+      backgroundPosition: '50% 50%',
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover'
+    };
+  }
+
+  render() {
+    return(
+      <div className="blog__thumbnail" style={this.styleThumb()} onClick={this.props.clickHandler} data-post={this.props.clickId}>
+        <h2 data-post={this.props.clickId}>{this.props.title}</h2>
+        <p data-post={this.props.clickId}>{this.props.snippet}</p>
+      </div>
+    );
+  }
+}
+
+class FullPost extends React.Component {
+  constructor(props) {
+    super(props);
     this.renderImage = this.renderImage.bind(this);
   }
 
@@ -70,6 +106,8 @@ class Blog extends React.Component {
       activePost: false // The number of the post being fully displayed will go here!
     }
     this.drawBlog = this.drawBlog.bind(this);
+    this.drawThumbs = this.drawThumbs.bind(this);
+    this.setActivePost = this.setActivePost.bind(this);
   }
 
   drawBlog() {
@@ -78,15 +116,37 @@ class Blog extends React.Component {
     const drawnPosts = [];
     for (let i=0; i<keys.length; i++) {
       const thisPost = posts[keys[i]];
-      drawnPosts.push(<Post key={`post${keys[i]}`} title={thisPost.title} image={thisPost.image} imageOrientation={thisPost.imageOrientation} imageAlt={thisPost.imageAlt} snippet={thisPost.snippet} fullText={thisPost.fullText} />);
+      drawnPosts.push(<FullPost key={`post${keys[i]}`} title={thisPost.title} image={thisPost.image} imageOrientation={thisPost.imageOrientation} imageAlt={thisPost.imageAlt} snippet={thisPost.snippet} fullText={thisPost.fullText} />);
     }
     return drawnPosts;
+  }
+
+  drawThumbs() {
+    // I'm using a keys array and a for loop instead of for(item in object), because
+    // apparently for(i in o) returns in an unstable order across browsers, and I want
+    // to be sure that blog posts are presented in reverse chronological order!
+    const posts = this.props.posts;
+    const keys = Object.keys(posts);
+    const drawnThumbs = [];
+    const activePost = this.state.activePost;
+    for (let i=keys.length-1; i>=0; i--) {
+      // iterating backwards to support reverse chronological ordering of posts
+      const thisPost = posts[keys[i]];
+      const idKey = `postThumb${i}`;
+      drawnThumbs.push(<ThumbPost key={idKey} title={thisPost.title} image={thisPost.image} snippet={thisPost.snippet} clickHandler={this.setActivePost} clickId={i} active={this.state.activePost === i ? true : false} />)
+    }
+    return drawnThumbs;
+  }
+
+  setActivePost(event) {
+    const clicked = Number(event.target.dataset.post);
+    this.setState( { activePost: clicked } );
   }
 
   render() {
     return (
       <div className="blog__container">
-        {this.drawBlog()}
+        {this.drawThumbs()}
       </div>
     );
   }
